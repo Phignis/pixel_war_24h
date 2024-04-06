ffcd2123f7aba7bc4fe45f6fc49a1091ce6b4136
stavan-blanchard <stevan.blanchard@gmail.com>
2024-04-06 15:47:20 +0200

    Worker class

---
+++ maven_src/src/main/java/fr/ensim/charme_quartier/pixel_war/model/WorkerType.java
@@ -0,0 +1,31 @@
+package fr.ensim.charme_quartier.pixel_war.model;
+
+public class WorkerType {
+    private String type;
+    private String description;
+    private int cooldown;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCooldown() {
+        return cooldown;
+    }
+
+    public void setCooldown(int cooldown) {
+        this.cooldown = cooldown;
+    }
+}
