580b97fe0c12ce1bcd01ffd598a9071a7e3113dc
Phignis <baptiste63.foucras.pro@gmail.com>
2024-04-06 11:35:02 +0200

    add basic maven project

---
+++ maven_src/src/main/java/fr/ensim/charme_quartier/pixel_war/PixelWarApplication.java
@@ -0,0 +1,13 @@
+package fr.ensim.charme_quartier.pixel_war;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PixelWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PixelWarApplication.class, args);
+	}
+
+}
---
+++ maven_src/src/test/java/fr/ensim/charme_quartier/pixel_war/PixelWarApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ensim.charme_quartier.pixel_war;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
